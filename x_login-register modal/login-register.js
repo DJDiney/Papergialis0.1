80e282f13c8550cafb7cb140d8a2117da467cb0f
DJDiney <rickeight@hotmail.com>
2016-06-24 20:52:30 -0300

    Papergialis0.1b

--- x_login-register modal/login-register.js
+++ x_login-register modal/login-register.js
@@ -56,6 +56,9 @@ function loginAjax(){
     */
 
 /*   Simulate error message from the server   */
+	if(document.getElementById("email").value=="admin"){
+		window.location.href = "Principal.html"
+	}
      shakeModal();
 }
 
